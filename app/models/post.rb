11dc1a78d457f3f5e0c63f3c0c98a4811bb1f5e7
Chaim Chazan <chaimyisroel@gmail.com>
2021-10-12 12:52:06 -0400

    commit tree 4fd0382cb26a2179306f88736ed85920d6d5a756

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+   validates :title, presence: true
+   validates :content, length: { minimum: 250 }
+   validates :summary, length: { maximum: 250 }
+   validates :category, inclusion: ['Fiction', 'Non-Fiction']
+   
+
+   validate :title_include
+
+
+   def title_include
+      unless title.present? && (title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top [number]") || title.include?("Guess"))
+        errors.add :title, "Need a title"
+      end
+    end
+   
 end
